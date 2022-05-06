594df939a0528417027cfc8bf83a5e3ef32dc0ba
ruben rodriguez <rotap45r@gmail.com>
2022-05-06 14:45:00 -0400

    registered music model with admin site and completed post function for music

--- music/serializers.py
+++ music/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Music
 
-class musicserializer(serializers.ModelSerializer):
-    class meta:
+class Musicserializer(serializers.ModelSerializer):
+    class Meta:
         model = Music
         fields = ['id','title','artist','album','release_date','genre' ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Music
+from .serializers import Musicserializer
 # Create your views here.
-@api_view(['GET'])
+
+@api_view(['GET','POST'])
 def music_list(request):
-    
-   return Response('ok')
+
+   if request.method == 'GET':
+    music = Music.objects.all()
+    serializer = Musicserializer(music, many=True)
+    return Response(serializer.data)
+
+
+   elif request.method == 'POST':
+      serializer = Musicserializer(data=request.data)
+      if serializer.is_valid()==True:
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+      else:
+          Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
